11c4ccd59c74580b5f24e2c8e6169d51530943c5
Michal Trykoszko <trykoszkom@gmail.com>
2021-01-08 08:08:04 +0100

    add build to repo

---
+++ antyki/public/component---src-pages-404-js-8a89e1aaa51d833631bf.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{w2l6:function(t,e,n){"use strict";n.r(e);var a=n("q1tI"),o=n.n(a),l=n("Zttt");e.default=function(t){var e=t.location;return o.a.createElement(l.a,{location:e},o.a.createElement("h1",null,"NOT FOUND"),o.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-8a89e1aaa51d833631bf.js.map
